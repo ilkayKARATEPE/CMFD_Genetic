2dc6e963429fda71830bc6340c4a89fc01383dc0
ilkay <ilkaykaratepe@gmail.com>
2019-12-06 04:32:05 +0300

    Bitti gibi :)

--- Genetic.py
+++ Genetic.py
@@ -1,35 +1,72 @@
 import numpy as np
-import pandas as pd
 
 
 class Genetic:
 
     def __init__(self, number_of_generations, df):
         self.number_of_generations = number_of_generations
-        self.first_generation_DataFrame = df
+        self.actual_dataFrame = df
         self.cursor = 1
         df.to_csv('mask.csv', sep=';', encoding='utf-8')
 
     def get_generation(self):
-        df = self.first_generation_DataFrame
+        df = self.actual_dataFrame
         mask_array_string = df[df.generation == self.cursor & df.selection]['mask']
         arr = []
+
         for sub_array in mask_array_string:
             arr.append(self.string_to_matrix(sub_array))
 
         return arr
 
+    def get_actual_generation_index(self):
+        df = self.actual_dataFrame
+        return df.index[df.generation == self.cursor & df.selection].tolist()
+
     def generate_new_population(self):
-        print()
+        self.cursor += 1
+        ayni_boyut_varmi = False
+        df = self.actual_dataFrame
+        for index_dis, gen_mask_dis in enumerate(self.get_generation()):
+            for index_ic, gen_mask_ic in enumerate(self.get_generation()):
+                if index_ic == index_dis:
+                    continue
+                else:
+                    if gen_mask_dis.shape == gen_mask_ic.shape:
+                        ayni_boyut_varmi = True
+                        new_gen = np.multiply(gen_mask_ic, gen_mask_dis)
+                        new_gen /= new_gen.sum(axis=1)[:, np.newaxis]  # normalize
+                        df = df.append({'mask': self.matrix_to_string(new_gen), 'generation': self.cursor,
+                                        'match': 0, 'selection': True}, ignore_index=True)
+        if ayni_boyut_varmi:
+            df.to_csv('mask.csv', sep=';', encoding='utf-8')
+        return ayni_boyut_varmi
 
     def selection(self):
-        print()
+        df = self.actual_dataFrame
+        matched = df[df.generation == self.cursor & df.selection]['match']
+        print(matched)
+        if matched.mean() < 5:
+            return False
+        else:
+            df.loc[df['match'] < matched.mean(), "selection"] = False
+            df.to_csv('mask.csv', sep=';', encoding='utf-8')
+            return False
 
     def string_to_matrix(self, str_):
         if str_.count(',') == 8:
             return np.array(list(map(float, str_.split(',')))).reshape(3, 3)
+
         elif str_.count(',') == 24:
             return np.array(list(map(float, str_.split(',')))).reshape(5, 5)
-        
+
         elif str_.count(',') == 48:
             return np.array(list(map(float, str_.split(',')))).reshape(7, 7)
+
+    def matrix_to_string(self, matrix):
+        b = ''
+        for i in range(0, matrix.shape[0]):
+            for j in range(0, matrix.shape[1] - 1):
+                b += str(matrix[i, j]) + ','
+
+        return b[:-1]
--- Main.py
+++ Main.py
@@ -1,22 +1,41 @@
-import SiftMach
-import Genetic
 import cv2 as cv
-import numpy as np
 import pandas as pd
 
-img_gray = cv.imread("im48_t.bmp", cv.IMREAD_GRAYSCALE)
-img_rgb = cv.imread("im48_t.bmp", cv.IMREAD_COLOR)
+import Genetic
+import SiftMach
 
+img_gray = cv.imread("im9_t.bmp", cv.IMREAD_GRAYSCALE)
+img_rgb = cv.imread("im9_t.bmp", cv.IMREAD_COLOR)
 df = pd.read_csv('masks_backup.csv', sep=';', index_col=['id'])
 
 siftmach_class = SiftMach.SiftMach(150, 40)
 genetic_class = Genetic.Genetic(3, df)
 
-for index, gen_mask in enumerate(genetic_class.get_generation()):
-    destination_gray_img = cv.filter2D(img_gray, -1, gen_mask)
-    x = siftmach_class.apply_sift(destination_gray_img, img_rgb)
-    cv.imshow("mask: " + str(index), destination_gray_img)
-    print("index: {} →→ eslesen {} nokta".format(index + 1, x))
+while True:
+    for index, gen_mask in enumerate(genetic_class.get_generation()):
+        destination_gray_img = cv.filter2D(img_gray, -1, gen_mask)
+        amount_of_match = siftmach_class.apply_sift(destination_gray_img, img_rgb)
+        print(genetic_class.get_actual_generation_index()[index])
+        df.loc[genetic_class.get_actual_generation_index()[index], "match"] = amount_of_match
+
+        # cv.imshow("mask: " + str(index), destination_gray_img)
+
+        print("index: {} →→ eslesen {} nokta".format(index + 1, amount_of_match))
+
+    df.to_csv('mask.csv', sep=';', encoding='utf-8')
+    if not genetic_class.selection():
+        break
+    else:
+        if not genetic_class.generate_new_population():
+            break
+
+df_final = pd.read_csv('mask.csv', sep=';', index_col=['id'])
+best_mask = genetic_class.string_to_matrix(df_final.loc[df_final['match'].idxmax()]['mask'])
+print(best_mask)
+
+destination_gray_img = cv.filter2D(img_gray, -1, best_mask)
+siftmach_class.apply_sift_final(destination_gray_img, img_rgb)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
